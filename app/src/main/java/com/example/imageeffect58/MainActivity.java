14bf3f2f8daad00abc5284ff5c07bd12eab9a3bc
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-03 20:59:50 +0100

    60 Removed boilerplate Floating Action Button (carefully - in activity_main design GUI & MainActivity.java onCreate)

--- app/src/main/java/com/example/imageeffect58/MainActivity.java
+++ app/src/main/java/com/example/imageeffect58/MainActivity.java
@@ -31,16 +31,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-                FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
         // image processing in order to perform effect on image
         imageView = (ImageView) findViewById(R.id.image_view);
 
